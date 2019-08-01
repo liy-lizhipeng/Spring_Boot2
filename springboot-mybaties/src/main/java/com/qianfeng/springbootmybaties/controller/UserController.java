ace5eb0284f1456d6c5fa33ecf0ffa80a4109ad2
lizhipeng <3211726673@qq.com>
2019-08-01 17:20:49 +0800

    添加了一个test方法

--- springboot-mybaties/src/main/java/com/qianfeng/springbootmybaties/controller/UserController.java
+++ springboot-mybaties/src/main/java/com/qianfeng/springbootmybaties/controller/UserController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    public void test(){};
     @Autowired
     private IUserService userService;
 
